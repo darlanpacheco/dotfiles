21a67dce1b6e2dc5c66e22deabfa2083e2cb0f5b
Darlan Pacheco <143919135+darlanpacheco@users.noreply.github.com>
2024-07-18 02:27:04 +0000

    initial commit

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<C-Right>", "<C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>h", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dg", ":lua vim.diagnostic.setloclist()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>df", ":lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tt", ":Telescope<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
---
+++ os/windows-setup.bat
@@ -0,0 +1,8 @@
+winget upgrade
+
+winget install Valve.Steam EpicGames.EpicGamesLauncher
+winget install Mozilla.Firefox OBSProject.OBSStudio
+
+winget upgrade
+
+wsl --install -d debian
