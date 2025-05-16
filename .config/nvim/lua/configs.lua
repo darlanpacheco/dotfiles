f7e2c768c7b899efe016e311237c5e5138c36bf2
Darlan Pacheco <darlanpacheco@proton.me>
2025-05-16 20:08:32 +0000

    initial commit

---
+++ .config/nvim/lua/configs.lua
@@ -0,0 +1,50 @@
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.wrap = true
+vim.opt.cursorline = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 10
+vim.opt.laststatus = 0
+vim.g.rustfmt_autosave = true
+vim.g.dart_format_on_save = true
+
+vim.diagnostic.config({
+	virtual_text = false,
+	virtual_lines = true,
+	update_in_insert = true,
+	signs = false,
+	underline = false,
+})
+
+_G.lsps = {
+	"rust_analyzer",
+	"clangd",
+	"bashls",
+	"html",
+	"cssls",
+	"ts_ls",
+	"tailwindcss",
+	"lua_ls",
+}
+_G.formatters = {
+	c = { "clang-format" },
+	cpp = { "clang-format" },
+	bash = { "shfmt" },
+	html = { "prettier" },
+	css = { "prettier" },
+	javascript = { "prettier" },
+	typescript = { "prettier" },
+	javascriptreact = { "prettier" },
+	typescriptreact = { "prettier" },
+	json = { "prettier" },
+	lua = { "stylua" },
+}
+_G.border_style = "single"
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
