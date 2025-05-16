e7cb3dcf453ae4e59f4a0a070543a5927dc1ab21
Darlan Pacheco <darlanpacheco@proton.me>
2025-05-16 20:08:32 +0000

    initial commit

---
+++ os/packages-install.sh
@@ -0,0 +1,5 @@
+sudo pacman -Suy --needed pipewire-pulse jack2 vulkan-radeon lib32-vulkan-radeon wine wine-mono libappimage unzip gstreamer qt6-multimedia-gstreamer gst-plugins-base gst-plugins-ugly gst-plugins-bad gst-plugins-good
+sudo pacman -Suy --needed xdg-desktop-portal-hyprland wl-clipboard openssh ufw fastfetch nodejs dotnet-sdk go npm yarn
+sudo pacman -Suy --needed wayland hyprland waybar swaybg hyprpicker gammastep slurp grim alacritty rofi-wayland pavucontrol ranger bashtop totem decibels loupe fragments gnome-boxes gnome-tweaks
+sudo pacman -Suy --needed flatpak firefox chromium godot-mono gimp krita inkscape blender lmms obs-studio libreoffice-fresh docker docker-compose mangohud steam lutris
+sudo pacman -Suy --needed papirus-icon-theme ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji
